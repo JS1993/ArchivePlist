cbae894708007b6eed52e3cb4dded8c90d619247
susu <28025920@qq.com>
2016-04-19 20:52:26 +0800

    Archive array to plist.

--- ArchivePlist/ArchivePlist/ViewController.m
+++ ArchivePlist/ArchivePlist/ViewController.m
@@ -16,12 +16,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //创建归档对象
+    NSArray* arr=@[@1,@2,@3];
+    //创建归档路径
+    NSString* path=NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES)[0];
+    //设置文件路径
+    NSString* filePath=[path stringByAppendingPathComponent:@"arr.plist"];
+    //写入文件
+    [arr writeToFile:filePath atomically:YES];
+    
+    NSLog(@"%@",path);
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
